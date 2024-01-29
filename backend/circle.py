f5780347daed2482e0fef501d999818bea27cc8f
sth0642 <sth0642@wmich.edu>
2024-01-29 11:06:22 -0500

    added line and circle class

---
+++ backend/circle.py
@@ -0,0 +1,5 @@
+class Circle:
+    def __init__(self, center, radius) -> None:
+        # center is a Point class value
+        self.center = center
+        self.radius = radius
---
+++ backend/line.py
@@ -0,0 +1,5 @@
+class Line:
+    def __init__(self, start, end) -> None:
+        # start and end are both Point class values
+        self.start = start
+        self.end = end
