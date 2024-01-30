79f6acea8aceff01b546baec3336c5868d964a0d
sth0642 <sth0642@wmich.edu>
2024-01-30 15:22:05 -0500

    User can now enter number of points & formatting fixes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 from backend.point import Point
 from backend.line import Line
@@ -13,6 +13,7 @@ import random
 
 app = Flask(__name__)
 
+
 # Main Index Page
 @app.route('/', methods=['POST', "GET"])
 def index():
@@ -22,33 +23,43 @@ def index():
 
     if request.method == 'POST':
         option = request.form['options']
+        point_num = request.form['point_num']
+
+        # If user didn't given a number of points, re-render index page
+        if point_num == '':
+            return render_template('index.html', point_data=point_data,
+                                   line_data=line_data,
+                                   circle_data=circle_data,)
+
+        # Create number of points given by user
+        points = random_points(int(point_num))
 
         # Based on user entered option, call appropriate function
         if option == 'op1':
-            point_data, line_data, circle_data = line_seg()
+            point_data, line_data, circle_data = line_seg(points)
         elif option == 'op2':
-            point_data, line_data, circle_data = closest_pair()
+            point_data, line_data, circle_data = closest_pair(points)
         elif option == 'op3':
-            point_data, line_data, circle_data = convex_hull()
+            point_data, line_data, circle_data = convex_hull(points)
         elif option == 'op4':
-            point_data, line_data, circle_data = largest_circle()
+            point_data, line_data, circle_data = largest_circle(points)
         else:
             print('invalid option')
 
         return render_template('index.html', point_data=point_data,
-                                             line_data=line_data,
-                                             circle_data=circle_data,)
+                               line_data=line_data,
+                               circle_data=circle_data,)
     else:
         return render_template('index.html', point_data=point_data,
-                                             line_data=line_data,
-                                             circle_data=circle_data,)
+                               line_data=line_data,
+                               circle_data=circle_data,)
 
 
-def line_seg():
+def line_seg(points):
     Line_Seg.test()
 
-    # Generate random point, line, and circle data
-    points, lines, circles = random_data()
+    # Generate random line, and circle data
+    lines, circles = random_data()
 
     # Put point, line, and circle data into json format
     point_data, line_data, circle_data = data_into_json(points, lines, circles)
@@ -56,11 +67,11 @@ def line_seg():
     return point_data, line_data, circle_data
 
 
-def closest_pair():
+def closest_pair(points):
     Closest_Pair.test()
 
     # Generate random point, line, and circle data
-    points, lines, circles = random_data()
+    lines, circles = random_data()
 
     # Put point, line, and circle data into json format
     point_data, line_data, circle_data = data_into_json(points, lines, circles)
@@ -68,11 +79,11 @@ def closest_pair():
     return point_data, line_data, circle_data
 
 
-def convex_hull():
+def convex_hull(points):
     Convex_Hull.test()
 
     # Generate random point, line, and circle data
-    points, lines, circles = random_data()
+    lines, circles = random_data()
 
     # Put point, line, and circle data into json format
     point_data, line_data, circle_data = data_into_json(points, lines, circles)
@@ -80,18 +91,18 @@ def convex_hull():
     return point_data, line_data, circle_data
 
 
-def largest_circle():
+def largest_circle(points):
     Largest_Circle.test()
 
     # Generate random point, line, and circle data
-    points, lines, circles = random_data()
+    lines, circles = random_data()
 
     # Put point, line, and circle data into json format
     point_data, line_data, circle_data = data_into_json(points, lines, circles)
 
     return point_data, line_data, circle_data
- 
-    
+
+
 # Function returns x random points
 def random_points(x):
     # Create x random points
@@ -105,20 +116,18 @@ def random_points(x):
 
 # Function creates random points, lines, and circles
 def random_data():
-    # Generate 50 random points, 2 random lines, and 2 random circles
-    points = random_points(50)
-
+    # Generate 2 random lines, and 2 random circles
     lines = [Line(Point(random.randint(10, 490), random.randint(10, 490)),
-                  Point(random.randint(10, 490), random.randint(10, 490))), 
+                  Point(random.randint(10, 490), random.randint(10, 490))),
              Line(Point(random.randint(10, 490), random.randint(10, 490)),
                   Point(random.randint(10, 490), random.randint(10, 490))),]
-    
+
     circles = [Circle(Point(random.randint(10, 490), random.randint(10, 490)),
                       random.randint(50, 100)),
                Circle(Point(random.randint(10, 490), random.randint(10, 490)),
                       random.randint(50, 100)),]
-    
-    return points, lines, circles
+
+    return lines, circles
 
 
 # Function turns given point, line, and circle data into json format
