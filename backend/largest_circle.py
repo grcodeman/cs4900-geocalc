d8aa62635a310968d277c8aecb382f0959cff783
Matthew Schaney <matt@schaney.net>
2024-02-06 06:40:25 -0500

    Update largest_circle.py
    * Add the algorithm to compute the largest empty circle in a given set of points

--- backend/largest_circle.py
+++ backend/largest_circle.py
@@ -1,6 +1,29 @@
-class Largest_Circle:
-    def __init__(self) -> None:
-        pass
+import numpy as np
+from scipy.spatial import Delaunay
 
-    def test():
-        return print("Hello World!!!")
+
+class LargestEmptyCircle:
+    def __init__(self, points):
+        self.points = np.array(points)
+        self.delaunay = Delaunay(points)
+
+    def find_largest_empty_circle(self):
+        max_radius = 0
+        best_circle = None
+
+        for simplex in self.delaunay.simplices:
+            # Calculate the circumcenter and radius of the circumcircle for each simplex (triangle)
+            pts = self.points[simplex, :]
+            A = np.linalg.det(np.c_[pts, np.ones((3, 1))])
+            center = np.linalg.det(np.c_[np.sum(pts*pts, axis=1), pts[:,1], np.ones((3, 1))])
+            center = np.array([center, np.linalg.det(np.c_[pts[:,0], np.sum(pts*pts, axis=1),
+                                                           np.ones((3, 1))])]) / (2.0 * A)
+
+            radius = np.sqrt(np.sum(np.square(pts[0] - center)))
+
+            # Check if the circle is empty and larger than the current largest
+            if radius > max_radius and all(radius < np.linalg.norm(p - center) for p in self.points):
+                max_radius = radius
+                best_circle = (center, radius)
+
+        return best_circle
