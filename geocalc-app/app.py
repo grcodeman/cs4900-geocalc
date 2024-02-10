b48782e13231616eb0fc433cf7fc1e310810aa8b
sth0642 <sth0642@wmich.edu>
2024-02-10 18:44:58 -0500

    Add help command and PEP8 on app.py

--- geocalc-app/app.py
+++ geocalc-app/app.py
@@ -1,6 +1,5 @@
 # Standard library imports.
 import os
-import random
 import sys
 import numpy as np
 
@@ -32,23 +31,25 @@ from geocalc_lib.algorithms.line_segment import LineSegmentIntersection
 
 app = Flask(__name__)
 
-
 # Initialize points, lines, and circles as empty arrays
 points = []
 lines = []
 circles = []
 # Keeps track of if points and lines should be highlighted
 is_highlighted = [[], []]
-
+# Also keep track of grid size
 grid_size = [10]
 
-# Main Index Page
+
 @app.route('/', methods=['POST', "GET"])
 def index():
+    """
+    Main index page, calls functions based on user input.
+    """
     point_data = {}
     line_data = {}
     circle_data = {}
-    msg = ""
+    msg = ["Enter 'help' to view commands."]
 
     # Remove any previously made circles
     # and highlighted points and lines
@@ -57,66 +58,88 @@ def index():
 
     for i in range(len(is_highlighted[0])):
         is_highlighted[0][i] = False
-
     for i in range(len(is_highlighted[1])):
         is_highlighted[1][i] = False
 
     if request.method == 'POST':
+        # Get user inputted command
         command = request.form['command']
 
         # Call appropriate function based on command input
         if command.startswith('add_point'):
-            msg = add_point(command)
+            msg = [add_point(command)]
         elif command.startswith('remove_point'):
-            msg = remove_point(command)
+            msg = [remove_point(command)]
         elif command.startswith("clear_points"):
-            msg = clear_points()
+            msg = [clear_points()]
         elif command.startswith("add_line"):
-            msg = add_line(command)
+            msg = [add_line(command)]
         elif command.startswith("remove_line"):
-            msg = remove_line(command)
+            msg = [remove_line(command)]
         elif command.startswith("clear_lines"):
-            msg = clear_lines()
+            msg = [clear_lines()]
         elif command.startswith("closest_pair_of_points"):
-            msg = closest_pair()
+            msg = [closest_pair()]
         elif command.startswith('convex_hull'):
-            msg = convex_hull()
+            msg = [convex_hull()]
         elif command.startswith('largest_empty_circle'):
-            msg = largest_circle()
-        elif command.startswith('line_segment'):
-            msg = line_segment()
+            msg = [largest_circle()]
+        elif command.startswith('line_intersections'):
+            msg = [line_intersections()]
         elif command.startswith('set_grid'):
-            msg = set_grid(command)
+            msg = [set_grid(command)]
+        elif command.startswith('help'):
+            # Display help message with commands
+            msg = ["Add a point or line: add_point x y |"
+                   + " add_line x1 y1 x2 y2",
+                   "Remove a point or line: remove_point x y |"
+                   + " remove_point x1 y1 x2 y2",
+                   "Clear all points or lines: clear_points | clear lines",
+                   "Algorithms: closest_pair_of_points | convex_hull | "
+                   + "largest_empty_circle | "
+                   + "line_intersections",
+                   "Change grid size: set_grid d"]
         else:
-            msg = "Invalid Command."
+            msg = ["Invalid Command."]
 
-        point_data, line_data, circle_data = data_into_json() 
+        # Turn points, lines, and circles into json data
+        point_data, line_data, circle_data = data_into_json()
 
+        # Render index.html with supplied data
         return render_template('index.html', point_data=point_data,
                                line_data=line_data,
                                circle_data=circle_data,
                                msg=msg, grid_size=grid_size[0])
     else:
+        # Render index.html with supplied data
         return render_template('index.html', point_data=point_data,
                                line_data=line_data,
                                circle_data=circle_data,
                                msg=msg, grid_size=grid_size[0])
 
+
 def add_point(command):
+    """
+    Adds a point to points array.
+    """
     try:
         # Parse command
         _, x, y = command.split()
         # Turn x and y into integers and create a Point class variable
         point = Point(int(x), int(y))
-        # Add point to points
+        # Add point to points is is_highlighted as False
         points.append(point)
         is_highlighted[0].append(False)
     except Exception as e:
         return f"Error adding point: {e}"
-    
+
     return f"{point} added."
 
+
 def remove_point(command):
+    """
+    Removes a given point from points array.
+    """
     try:
         # Parse command
         _, x, y = command.split()
@@ -124,26 +147,34 @@ def remove_point(command):
         point = Point(int(x), int(y))
         # Find the index of the point in points array.
         index = points.index(point)
-        # Remove the point from the points array.
+        # Remove the point from the points array and highlighted value.
         del points[index]
         del is_highlighted[0][index]
     except Exception as e:
         return f"Error removing point: {e}"
-    
+
     return f"{point} removed"
 
+
 def clear_points():
+    """
+    Removes all points from points array.
+    """
     try:
-        # Remove each point in points.
+        # Remove each point in points array and is_highlighted value.
         for i in range(len(points)-1, -1, -1):
             del points[i]
             del is_highlighted[0][i]
     except Exception as e:
         return f"Error clearing point: {e}"
-    
+
     return f"Cleared points."
 
+
 def add_line(command):
+    """
+    Adds a line to lines array.
+    """
     try:
         # Parse command
         _, x1, y1, x2, y2 = command.split()
@@ -154,10 +185,14 @@ def add_line(command):
         is_highlighted[1].append(False)
     except Exception as e:
         return f"Error adding line: {e}"
-    
+
     return f"{line} added"
 
+
 def remove_line(command):
+    """
+    Removes a given line from lines array.
+    """
     try:
         # Parse command
         _, x1, y1, x2, y2 = command.split()
@@ -170,21 +205,30 @@ def remove_line(command):
         del is_highlighted[1][index]
     except Exception as e:
         return f"Error removing line: {e}"
-    
+
     return f"{line} removed."
 
+
 def clear_lines():
+    """
+    Removes every line from lines array.
+    """
     try:
-        # Remove each line in lines.
+        # Remove each line in lines and its is_highlighted value.
         for i in range(len(lines)-1, -1, -1):
             del lines[i]
             del is_highlighted[1][i]
     except Exception as e:
         return f"Error clearing lines: {e}"
-    
+
     return f"Cleared lines."
 
+
 def closest_pair():
+    """
+    Function performs the closest pair of points algorithm on all
+    points and displays the closest pair of points as highlighted.
+    """
     try:
         # Turn points in np array
         np_points = np.array(points)
@@ -194,15 +238,15 @@ def closest_pair():
         closest_pair_finder = ClosestPairOfPoints(np_points)
         min_distance, best_pair = closest_pair_finder.closest_util(
             np_points)
-        
+
         # Highlight the closest pair of points
         for i in range(len(points)):
             if points[i] in best_pair:
                 is_highlighted[0][i] = True
-        
     except Exception as e:
         return f"Error finding closest pair of points: {e}"
-    
+
+    # Generate output message
     msg = f"({best_pair[0].coords[0]}, " \
           + f"{best_pair[0].coords[1]}) " \
           + f"and ({best_pair[1].coords[0]}, "\
@@ -212,13 +256,18 @@ def closest_pair():
 
     return msg
 
+
 def convex_hull():
+    """
+    Function performs the convex hull algorithm on all points
+    and creates the convex hull with lines connecting each hull point.
+    """
     try:
         # Turn points into array of tuples
         tuple_points = [(point.coords[0], point.coords[1])
                         for point in points]
 
-        # Initialize Convex_Hull and do a graham scan on all points
+        # Initialize ConvexHull and do a graham scan on all points
         ch = ConvexHull(tuple_points)
         hull = ch.graham_scan(tuple_points)
 
@@ -233,24 +282,36 @@ def convex_hull():
 
     return f"Successfully created convex hull out of {len(hull)-1} points."
 
+
 def largest_circle():
+    """
+    Function performs the largest empty circle algorithm on all points
+    and creates the largest empty circle to be displayed.
+    """
     try:
         # Turn points into np.array of array point values
         np_points = np.array([[point.coords[0], point.coords[1]]
-                                for point in points])
-        
+                              for point in points])
+
         # Run LargestEmptyCircle algorithm
         lec = LargestEmptyCircle(np_points)
         center, radius = lec.find_largest_empty_circle()
 
-        circles.append(Circle(Point(float(center[0]), float(center[1])), float(radius)))
+        # Create largest empty circle
+        circles.append(Circle(Point(float(center[0]),
+                                    float(center[1])), float(radius)))
     except Exception as e:
         return f"Error finding largest empty circle: {e}"
 
     return f"Largest empty circle has a center of"\
            + f" {center} and radius of {radius:.3f}."
 
-def line_segment():
+
+def line_intersections():
+    """
+    Function performs the line segment intersection algorithm on all
+    lines and highlights all intersecting lines.
+    """
     try:
         # Get each pair of lines
         line_pairs = []
@@ -263,10 +324,10 @@ def line_segment():
         results = []
         for pair in line_pairs:
             result = lsi.do_intersect(pair[0].start, pair[0].end,
-                                        pair[1].start, pair[1].end)
+                                      pair[1].start, pair[1].end)
             results.append(result)
 
-        # Keep track of lines that intersect so they can be highlighted
+        # Highlight each line that intersects with another line.
         for i in range(len(line_pairs)):
             if results[i]:
                 for j in range(len(lines)):
@@ -283,7 +344,11 @@ def line_segment():
 
     return f"{intersect_count} lines intersect."
 
+
 def set_grid(command):
+    """
+    Function changes the grid size to the given user input.
+    """
     try:
         # Parse command
         _, new_size = command.split()
@@ -297,8 +362,11 @@ def set_grid(command):
 
     return f"Successfully set new grid size to {grid_size}"
 
-# Function turns given point, line, and circle data into json format
+
 def data_into_json():
+    """
+    Function turns given point, line, and circle data into json format.
+    """
     point_data = {
         "x": [int(point.coords[0]) for point in points],
         "y": [int(point.coords[1]) for point in points],
